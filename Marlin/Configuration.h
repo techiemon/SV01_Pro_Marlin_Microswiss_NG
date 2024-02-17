7e2a16c51c41b7c2dce29b81569493a95d4f2ddf
John Hamson <jhamson@techiemon.com>
2024-02-17 08:15:19 -0600

    Adjust DEFAULT_AXIS_STEPS_PER_UNIT so 10mm of filament is feed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -933,7 +933,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 141 } // 137.6
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 390 } // 137.6
 
 /**
  * Default Max Feed Rate (mm/s)
